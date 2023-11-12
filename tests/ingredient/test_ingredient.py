f92bc7ad2b0e9e1c93371045f6789b38ecf27a58
Bárbara <babivaldez@gmail.com>
2023-11-12 16:49:05 -0300

    finaliza req.01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -5,9 +5,14 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 def test_ingredient():
     presunto = Ingredient("presunto")
     farinha = Ingredient("farinha")
+    presunto2 = Ingredient("presunto")
 
     assert presunto.name == "presunto"
     assert presunto.restrictions == {"ANIMAL_DERIVED", "ANIMAL_MEAT"}
     assert presunto.__repr__() != "presunto"
     assert presunto.__repr__() == "Ingredient(presunto)"
     assert presunto != farinha
+    assert presunto.__hash__() != farinha.__hash__()
+    assert presunto.__hash__() == presunto2.__hash__()
+    assert presunto.__eq__(farinha) is False
+    assert presunto.__eq__(presunto2) is True
