e89ea0c31f6f1a906e3d4105804f837513f51454
Bárbara <babivaldez@gmail.com>
2023-11-12 16:59:55 -0300

    acrescenta dados de restriction no test e corrige a linha 18

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -3,4 +3,14 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 # Req 2
 def test_dish():
-    pass
+    ingredient1 = Ingredient("salnão")
+    ingredient2 = Ingredient("farinha")
+    ingredient3 = Ingredient("queijo mussarela")
+    ingredient4 = Ingredient("ovo")
+    ingredient5 = Ingredient("bacon")
+
+    dish1 = Dish("Pizza", 45.0)
+    dish2 = Dish("Sanduiche", 27.0)
+
+    assert dish1.name == "Pizza"
+    assert dish1.price == 45.0
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,4 +1,5 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
@@ -6,11 +7,15 @@ def test_ingredient():
     presunto = Ingredient("presunto")
     farinha = Ingredient("farinha")
     presunto2 = Ingredient("presunto")
+    presunto_restrictions = {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.ANIMAL_MEAT,
+    }
 
     assert presunto.name == "presunto"
-    assert presunto.restrictions == {"ANIMAL_DERIVED", "ANIMAL_MEAT"}
+    assert presunto.restrictions == presunto_restrictions
     assert presunto.__repr__() != "presunto"
-    assert presunto.__repr__() == "Ingredient(presunto)"
+    assert presunto.__repr__() == "Ingredient('presunto')"
     assert presunto != farinha
     assert presunto.__hash__() != farinha.__hash__()
     assert presunto.__hash__() == presunto2.__hash__()
