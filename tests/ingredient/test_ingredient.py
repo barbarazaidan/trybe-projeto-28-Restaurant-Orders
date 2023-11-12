7229645712637ffe31602eb30adf36ac4183eac1
Bárbara <babivaldez@gmail.com>
2023-11-12 16:32:08 -0300

    iniciando o req.01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,11 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    presunto = Ingredient("presunto")
+    farinha = Ingredient("farinha")
+
+    assert presunto.name == "presunto"
+    assert presunto.restrictions == {"ANIMAL_DERIVED", "ANIMAL_MEAT"}
+    assert presunto.__repr__() != "presunto"
+    assert presunto.__repr__() == "Ingredient(presunto)"
+    assert presunto != farinha
